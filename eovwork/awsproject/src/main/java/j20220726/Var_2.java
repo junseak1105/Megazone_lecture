f0a8564d098dc671b6663b043ecf6306091599ce
junseak1105 <57184613+junseak1105@users.noreply.github.com>
2022-07-26 09:58:10 +0900

    BASE(2022.07.26)

---
+++ eovwork/awsproject/src/main/java/j20220726/Var_2.java
@@ -0,0 +1,15 @@
+package j20220726;
+
+public class Var_2 {
+	public static void main(String[] args) {
+		
+		String name = "김재훈";
+		String name2 = "멍멍이";
+		
+		name="홍길동";
+		
+		String strvalue="문자열";
+		System.out.println(strvalue);
+		
+	}
+}
