8daedc75660d2ba5fb39b9d27db0d66fa49d043a
junseak1105 <57184613+junseak1105@users.noreply.github.com>
2022-07-26 16:33:30 +0900

    2022-07-07
    강의 내용 및 퀴즈

--- eovwork/awsproject/src/main/java/j20220726/Var_3.java
+++ eovwork/awsproject/src/main/java/j20220726/Var_3.java
@@ -1,5 +1,11 @@
 package j20220726;
 
 public class Var_3 {
+	public static void main(String[] args) {
+		
+		int age = 26;
+		age +=10;
 
+		System.out.println("나이는:"+age);
+	}
 }
---
+++ eovwork/awsproject/src/main/java/questionpool/Person.java
@@ -0,0 +1,25 @@
+package questionpool;
+
+/*
+
+ * 1.작성자:김재훈
+
+ * 2.작성일:2022-07-26
+
+ * 3.수정일:2022-07-26
+
+ */
+public class Person {
+	public static void main(String[] args) {
+		
+		String name = "김재훈";
+		String address = "해운대구";
+		int age = 26;
+		age = age +10-20+20-20;;
+		
+		System.out.println("이름은:"+name);
+		System.out.println("나이는:"+age);
+		System.out.println("주소는:"+address);
+		;
+	}
+}
