338afa9d2b93ceb380014afc07b4fd7c300b21f1
김재훈 <57184613+junseak1105@users.noreply.github.com>
2022-09-01 18:57:03 +0900

    스프링 추가

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,13 @@
-package com.megazone.lecture;
+package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LectureApplication {
+public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LectureApplication.class, args);
+		SpringApplication.run(DemoApplication.class, args);
 	}
 
 }
